6e008d35dda6c163d86006794a04957d171f8db7
AntonioFBove <antoniofbove@gmail.com>
2022-01-14 18:58:36 +0100

    buttons js ok

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 new Vue ({
   el: "#app",
   data: {
+    pictures: [
+      'https://picsum.photos/id/1/1200/400',
+      'https://picsum.photos/id/2/1200/400',
+      'https://picsum.photos/id/3/1200/400',
+      'https://picsum.photos/id/4/1200/400',
+      'https://picsum.photos/id/5/1200/400',
+      'https://picsum.photos/id/6/1200/400',
+      'https://picsum.photos/id/7/1200/400',
+    ],
+    currentIndex: 0,
 
   },
   methods: {
-
+    prevPic: function() {
+      this.currentIndex = (this.currentIndex - 1) % this.pictures.length;
+      if(this.currentIndex < 0) {
+        this.currentIndex = this.pictures.length - 1;
+      }
+      console.log(this.currentIndex)
+    },
+    nextPic: function() {
+      this.currentIndex = (this.currentIndex + 1) % this.pictures.length;
+      console.log(this.currentIndex)
+    },
+    setIndex: function(e) {
+      const classes = e.target.classList;
+      console.log(classes);
+      // this.currentIndex = 
+    }
   }
 })
+
+// console.log(currentIndex);
